e78244f726a397ec96d3636a1ab83b252e16a6ce
Đào Minh Thuấn <53705853+daominhthuan42@users.noreply.github.com>
2025-03-12 19:34:40 +0700

    [SQL] Set up SQL

--- 01_TRUY_VAN_SELECT/TRUY_VAN_SELECT.sql
+++ 01_TRUY_VAN_SELECT/TRUY_VAN_SELECT.sql
@@ -1,5 +1,18 @@
-﻿-- VIẾT CÂU LỆNH SQL LẤY RA TÊN TẤT CẢ SẢN PHẨM
-USE NORTHWND
+﻿USE NORTHWND
 GO
 
-SELECT * FROM dbo.Products
+-- VIẾT CÂU LỆNH SQL LẤY RA TÊN TẤT CẢ SẢN PHẨM
+SELECT * FROM [dbo].[Products]
+
+-- VIẾT CÂU LỆNH SQL LẤY RA TÊN SẢN PHẨM, GIÁ BÁN, SỐ LƯỢNG SẢN PHẦM TRÊN ĐƠN VỊ
+SELECT PR.ProductName, PR.QuantityPerUnit, PR.UnitPrice
+FROM [dbo].[Products] AS PR
+
+-- VIẾT CÂU LỆNH SQL LẤY RA TÊN CÔNG TY KHÁCH HÀNG VÀ QUỐC GIA KHÁCH HÀNG ĐÓ 
+SELECT * FROM [dbo].[Customers]
+SELECT CU.CompanyName, CU.Country FROM [dbo].[Customers] AS CU
+
+-- VIẾT CÂU LỆNH SQL LẤY RA TÊN CÔNG TY VÀ SỐ ĐIỆN THOẠI CỦA TẤT CẢ CÁC NHÀ CUNG CẤP HÀNG.
+SELECT* FROM dbo.Suppliers
+SELECT SU.CompanyName, SU.Phone FROM dbo.Suppliers AS SU
+
