6fea30a60e774e062c6a6b59cb89f06f587213bf
Đào Minh Thuấn <53705853+daominhthuan42@users.noreply.github.com>
2025-04-08 17:55:58 +0700

    [SQL] TU HOC SQL

---
+++ 43_INDEX_HOWKTEAM/INDEX.sql
@@ -0,0 +1,28 @@
+﻿USE HowKteam
+GO
+
+--DECLARE @i INT = 1
+--WHILE (@i < 1000000)
+--BEGIN
+--	INSERT [dbo].[NGUOITHAN] (MAGV, TEN, NGSINH, PHAI)
+--	VALUES (
+--			N'001',
+--			N'TEST' + CAST(@i AS NCHAR(12)),
+--			GETDATE(),
+--			N'NAM'
+--			)
+--	SET @i += 1
+--END
+
+SELECT * FROM [dbo].[NGUOITHAN]
+
+-- Tạo index trên bảng giáo viên
+-- Tăng tốc độ tìm kím <> Chậm tốc độ thêm, xóa, sưa
+-- Cho phép các trường trùng nhau
+CREATE INDEX IndexName ON dbo.NGUOITHAN(MaGV)
+
+-- Không Cho phép các trường trùng nhau
+CREATE UNIQUE INDEX IndexNameUnique ON dbo.GIAOVIEN(MaGV)
+
+SELECT * FROM dbo.NGUOITHAN
+WHERE MAGV = '001' AND TEN = 'TEST999999'
---
+++ 45_DELARE_BIEN_HOWKTEAM/BIEN.sql
@@ -0,0 +1,57 @@
+﻿USE HowKteam
+GO
+
+-- TÌM RA MÃ GIÁO VIÊN CÓ LƯƠNG THẤP NHẤT.
+SELECT TOP 1 MAGV
+FROM [dbo].[GIAOVIEN]
+WHERE [LUONG] = (SELECT MIN(LUONG) FROM [dbo].[GIAOVIEN])
+
+-- LẤY RA TUỔI CỦA GIÁO VIÊN VỚI MÃ GIÁO VIÊN CÓ LƯƠNG THẤP NHẤT.
+SELECT YEAR(GETDATE()) - YEAR([NGSINH])
+FROM [dbo].[GIAOVIEN] 
+WHERE MAGV = 
+			(
+				SELECT TOP 1 MAGV
+				FROM [dbo].[GIAOVIEN]
+				WHERE [LUONG] = (SELECT MIN(LUONG) FROM [dbo].[GIAOVIEN])
+			);
+
+--------------------------------------------------------------------------
+-- TẠO RA 1 BIẾN KIỂU NCHAR LƯU MÃ GIÁO VIÊN LƯƠNG THẤP NHẤT
+DECLARE @minSalaryMAGV NCHAR(3)
+
+SELECT @minSalaryMAGV = MAGV FROM [dbo].[GIAOVIEN] WHERE [LUONG] = (SELECT MIN(LUONG) FROM [dbo].[GIAOVIEN])
+
+SELECT YEAR(GETDATE()) - YEAR([NGSINH])
+FROM [dbo].[GIAOVIEN] 
+WHERE MAGV = @minSalaryMAGV
+
+--------------------------------------------------------------------------
+-- KHỚI TẠO VỚI KIỂU DỮ LIỆU
+DECLARE @i INT
+
+-- KHỚI TẠO VỚI GIÁ TRỊ MẶC ĐỊNH
+DECLARE @j INT = 0
+
+-- SET DỮ LIỆU CHO BIẾN
+SET @i += 1
+SET @j += @i
+
+-- SET THÔNG QUA CÂU SELECT
+DECLARE @minLuong INT = 0
+SELECT @minLuong =  MIN(LUONG) FROM [dbo].[GIAOVIEN];
+
+----------------------------------------------------------------------------
+-- XUẤT RA SỐ LƯỢNG NGƯỜI THÂN CỦA GIÁO VIÊN 001
+-- LƯU MÃ GIÁO VIÊN 003 LẠI
+-- TÌM RA SỐ LƯỢNG NGƯỜI THÂN TƯƠNG ỨNG
+DECLARE @maGV NCHAR(3) = '003'
+SELECT COUNT(*) FROM [dbo].[NGUOITHAN]
+WHERE [MAGV] = @maGV;
+
+-- XUẤT RA TÊN GIÁO VIÊN CÓ LƯƠNG THẤP NHẤT
+DECLARE @minSlary INT
+
+SELECT @minSlary = MIN(LUONG) FROM [dbo].[GIAOVIEN]
+SELECT [HOTEN] FROM [dbo].[GIAOVIEN]
+WHERE [LUONG] = @minSlary;
